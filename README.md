51b280b5385690a84ce89b1a2aff8e587b4e3bfc
Creature101 <dev.kononov@gmail.com>
2019-08-21 03:45:28 +0300

    FUNCOIN MVP V1.0.0

---
+++ README.md
@@ -0,0 +1,21 @@
+## Go Funcoin
+
+Golang implementation of the FUNCOIN protocol.
+
+
+## Building the source
+
+For prerequisites and detailed build instructions please read the [Installation Instructions](https://#) on the wiki.
+
+Building `geth` requires both a Go (version 1.10 or later) and a C compiler. You can install
+them using your favourite package manager. Once the dependencies are installed, run
+
+```shell
+make geth
+```
+
+or, to build the full suite of utilities:
+
+```shell
+make all
+```
---
+++ params/bootnodes.go
@@ -0,0 +1,44 @@
+// Copyright 2015 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package params
+
+// MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
+// the main Ethereum network.
+var MainnetBootnodes = []string{
+	// FUNCOIN Foundation Go Bootnodes
+	"enode://1f280130654b4aadf8beb7b17906699a9572b53cde6a28e8f7359f93d3a8f5607766636e9395495b50ed577ddf5640d111c78f31689d809a0ffebe32158b7ef0@178.172.224.230:50505",
+	"enode://e7a8e522cc6a458819bbf971487b7041648a36312dd32b899efc9af5937ef65a17349cef9232eef90d0072d4b3d2267749c65df741df5386fbe2fa72fb4256a1@178.172.224.229:50505",
+	"enode://996cf3cf639f7a3353f8b82fca1af6c4102671a5a9481fe605250e2b4bc51704680377acbaa05e59d3c5a2e9b9b1c09e096523219555557505033d6d4f22b18b@5.79.124.129:50505",
+	"enode://a47a2ff2b4d4c8238688cf9754c66bf2bf2886dbf0675bc46ec7a53c605c21d58dc09b967402a5070601deec54e90f6375d45ab02f5500d3d93507c431c4fa46@162.210.197.194:50505",
+	"enode://74f31a99b0096391a5d06ef2f188827d83eee05291cec667411f1601d79d63a7023d66e07767d9b2bb99406e505ee93bc317f105f32eaba2b957eb10497f71c6@209.58.176.176:50505",
+}
+
+// TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
+// Ropsten test network.
+var TestnetBootnodes = []string{}
+
+// RinkebyBootnodes are the enode URLs of the P2P bootstrap nodes running on the
+// Rinkeby test network.
+var RinkebyBootnodes = []string{}
+
+// GoerliBootnodes are the enode URLs of the P2P bootstrap nodes running on the
+// Görli test network.
+var GoerliBootnodes = []string{}
+
+// DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the
+// experimental RLPx v5 topic-discovery network.
+var DiscoveryV5Bootnodes = []string{}
